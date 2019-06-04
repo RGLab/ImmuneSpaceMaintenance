ce97663f04c7acba2464153a2648c8f872dc3a00
Ju Yeong Kim <jkim2345@fredhutch.org>
2019-06-03 19:19:25 -0700

    Modify error message and change slack notification behavior

--- check.R
+++ check.R
@@ -5,7 +5,7 @@ labkey.netrc.file <- ImmuneSpaceR:::.get_env_netrc()
 labkey.url.base <- ImmuneSpaceR:::.get_env_url()
 
 con <- ISM$new("")
-res <- con$checkStudyCompliance()
+msg <- testthat::capture_message(res <- con$checkStudyCompliance())
 res
 
-testthat::expect_length(res, 0)
+testthat::expect_equal(length(res), 0, label = msg$message)
