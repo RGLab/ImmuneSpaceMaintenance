7e6aa759a044106f66241a0ae180850a7758e14d
Ju Yeong Kim <jkim2345@fredhutch.org>
2019-06-03 21:18:28 -0700

    Return a data.table instead of list to easily digest the results and give better error message

--- check.R
+++ check.R
@@ -6,6 +6,18 @@ labkey.url.base <- ImmuneSpaceR:::.get_env_url()
 
 con <- ISM$new("")
 msg <- testthat::capture_messages(res <- con$checkStudyCompliance())
-res
 
-testthat::expect_equal(length(res), 0, label = msg)
+if (length(res) > 0) {
+  dt <- data.table::as.data.table(do.call(rbind, res))
+  dt[, study := names(res)]
+  data.table::setcolorder(dt, c("study", "modules"))
+  dt
+
+  stop(
+    "\n",
+    length(res), " studies are not compliant: ",
+    paste(names(res), collapse = ", "),
+    "\n",
+    msg
+  )
+}
